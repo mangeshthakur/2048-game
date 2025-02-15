b341cd59c55e292efb38afc549d972bdd5d5978f
Pravesh-Sudha <88569706+Pravesh-Sudha@users.noreply.github.com>
2025-02-15 15:21:21 +0530

    Update main.tf

--- terraform-setup/main.tf
+++ terraform-setup/main.tf
@@ -13,6 +13,10 @@ resource "aws_instance" "jenkins_server" {
 
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "terraform-state-logs-pravesh"
+}
+
+resource "aws_s3_bucket_acl" "terraform_state_acl" {
+  bucket = aws_s3_bucket.terraform_state.id
   acl    = "private"
 }
 
