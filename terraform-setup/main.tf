0b63d44a7c4a60d4e0c8593302874d179fd8bd38
Pravesh-Sudha <88569706+Pravesh-Sudha@users.noreply.github.com>
2025-02-15 15:15:47 +0530

    Create main.tf

---
+++ terraform-setup/main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = "us-east-1"  # Change this to your preferred region
+}
+
+resource "aws_instance" "jenkins_server" {
+  ami           = "ami-04b4f1a9cf54c11d0" # Replace with the latest Ubuntu AMI
+  instance_type = "t2.small"
+  key_name      = "default-ec2.pem"  # Replace with your Key-name
+  tags = {
+    Name = "Terraform-Jenkins-Server"
+  }
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-state-logs-pravesh"
+  acl    = "private"
+}
+
+output "instance_public_ip" {
+  value = aws_instance.jenkins_server.public_ip
+}
