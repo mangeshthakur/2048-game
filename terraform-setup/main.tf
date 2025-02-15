5e403ac50a3ef1b824e5abc040a0d477620a8337
Pravesh-Sudha <88569706+Pravesh-Sudha@users.noreply.github.com>
2025-02-15 16:36:49 +0530

    Update main.tf

--- terraform-setup/main.tf
+++ terraform-setup/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "jenkins_server" {
   ami           = "ami-04b4f1a9cf54c11d0" # Replace with the latest Ubuntu AMI
   instance_type = "t2.small"
-  vpc_security_group_ids = "sg-0a76d30209f78079c" # Replace it with the security group of 2048-Jenkins-Terraform-Automation
+  vpc_security_group_ids = ["sg-0a76d30209f78079c"] # Replace it with the security group of 2048-Jenkins-Terraform-Automation
   key_name      = "default-ec2"  # Replace with your Key-name
   tags = {
     Name = "Terraform-Jenkins-Server"
