fd01f0990d55187de9d50dad2afb077590e42445
Pravesh-Sudha <88569706+Pravesh-Sudha@users.noreply.github.com>
2025-02-25 15:59:54 +0530

    Update main.tf

--- terraform-setup/main.tf
+++ terraform-setup/main.tf
@@ -12,17 +12,6 @@ resource "aws_instance" "jenkins_server" {
   }
 }
 
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-state-logs-pravesh"
-}
-
-resource "aws_s3_bucket_versioning" "versioning_example" {
-  bucket = aws_s3_bucket.terraform_state.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
 output "instance_public_ip" {
   value = aws_instance.jenkins_server.public_ip
 }
