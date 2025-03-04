a320fc7da690939aea96aaf75130cecfb54a9032
Kamal <113974679+devoperatr@users.noreply.github.com>
2025-03-04 12:58:09 +0530

    Update main.tf

--- terraform-setup/main.tf
+++ terraform-setup/main.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-  region = "us-east-1"  # Change this to your preferred region
+  region = "eu-central-1"  # Change this to your preferred region
 }
 
 resource "aws_instance" "jenkins_server" {
-  ami           = "ami-04b4f1a9cf54c11d0" # Replace with the latest Ubuntu AMI
+  ami           = "ami-07eef52105e8a2059" # Replace with the latest Ubuntu AMI
   instance_type = "t2.small"
-  vpc_security_group_ids = ["sg-0a76d30209f78079c"] # Replace it with the security group of 2048-Jenkins-Terraform-Automation
-  key_name      = "default-ec2"  # Replace with your Key-name
+  vpc_security_group_ids = ["sg-0acdfbd8a974c1058"] # Replace it with the security group of 2048-Jenkins-Terraform-Automation
+  key_name      = "master_jenkins"  # Replace with your Key-name
   tags = {
     Name = "Terraform-Jenkins-Server"
   }
